eae493f34151d792da8f1c2d55e9879b8fb111e8
Amie Corso <amie.corso@coinbase.com>
2025-03-25 08:10:32 -0700

    clean up permit2 test

--- test/src/PaymentEscrow/authorize/Permit2.t.sol
+++ test/src/PaymentEscrow/authorize/Permit2.t.sol
@@ -3,7 +3,53 @@ pragma solidity ^0.8.13;
 
 import {PaymentEscrowSmartWalletBase} from "../../../base/PaymentEscrowSmartWalletBase.sol";
 import {PaymentEscrow} from "../../../../src/PaymentEscrow.sol";
+import {MockERC20} from "solady/../test/utils/mocks/MockERC20.sol";
 
 contract AuthorizeWithPermit2Test is PaymentEscrowSmartWalletBase {
-// Tests for AuthorizationType.Permit2
+    // Non-ERC3009 token to guarantee no false positive use of Permit2 path
+    MockERC20 public plainToken;
+
+    function setUp() public override {
+        // Initialize PaymentEscrow with Permit2 first
+        super.setUp();
+
+        // Deploy a regular ERC20 without ERC3009
+        plainToken = new MockERC20("Plain Token", "PLAIN", 18);
+
+        // Buyer needs to approve Permit2 to spend their tokens
+        vm.startPrank(buyerEOA);
+        plainToken.approve(address(paymentEscrow.permit2()), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function test_succeeds_whenValueEqualsAuthorized(uint120 amount) public {
+        vm.assume(amount > 0);
+
+        // Mint enough tokens to the buyer
+        plainToken.mint(buyerEOA, amount);
+
+        PaymentEscrow.PaymentDetails memory paymentDetails = _createPaymentEscrowAuthorization({
+            buyer: buyerEOA,
+            value: amount,
+            token: address(plainToken),
+            authType: PaymentEscrow.AuthorizationType.Permit2
+        });
+
+        // Generate Permit2 signature using the same deadline as paymentDetails
+        bytes memory signature = _signPermit2Transfer({
+            token: address(plainToken),
+            amount: amount,
+            deadline: paymentDetails.authorizeDeadline,
+            nonce: uint256(keccak256(abi.encode(paymentDetails))),
+            privateKey: BUYER_EOA_PK
+        });
+
+        // Should succeed via Permit2 authorization
+        vm.prank(operator);
+        paymentEscrow.authorize(amount, paymentDetails, signature);
+
+        // Verify the transfer worked
+        assertEq(plainToken.balanceOf(address(paymentEscrow)), amount);
+        assertEq(plainToken.balanceOf(buyerEOA), 0);
+    }
 }
