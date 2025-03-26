0f75d2ca5df74877d7912f51f099997e6341f762
Conner Swenberg <conner.swenberg@coinbase.com>
2025-03-26 15:04:20 -0700

    Add sample Operator implementation

---
+++ src/Operator.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract Operator {
+    address escrow;
+    address owner;
+    mapping(address executor => bool allowed) allowedExecutors;
+
+    event ExecutorUpdated(address executor, bool allowed);
+
+    constructor(address escrow_, address owner_, address[] memory executors) {
+        escrow = escrow;
+        owner = owner_;
+        for (uint256 i; i < executors.length; i++) {
+            allowedExecutors[executors[i]] = true;
+        }
+    }
+
+    function sendCalls(bytes[] calldata calls) external {
+        if (!allowedExecutors[msg.sender]) revert();
+
+        uint256 callsLen = calls.length;
+        for (uint256 i; i < callsLen; i++) {
+            escrow.call(calls[i]);
+        }
+    }
+
+    function updateExecutor(address executor, bool allowed) external {
+        if (msg.sender != owner) revert();
+
+        allowedExecutors[executor] = allowed;
+        emit ExecutorUpdated(executor, allowed);
+    }
+}
