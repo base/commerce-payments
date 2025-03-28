fb22efd11ac8cfa8cdec14dfe61356a82b8d55d4
Conner Swenberg <conner.swenberg@coinbase.com>
2025-03-28 09:23:56 -0700

    Add call failure logs

--- src/Operator.sol
+++ src/Operator.sol
@@ -6,13 +6,20 @@ import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 /// @notice Minimal contract for PaymentEscrow operations
 /// @dev Enables batch execution from multiple executors for optimal throughput and efficiency
 contract Operator is Ownable {
-    address immutable escrow;
-    mapping(address executor => bool allowed) isExecutor;
+    struct Call {
+        bytes32 id;
+        address target;
+        bytes data;
+    }
+
+    mapping(address executor => bool allowed) public isExecutor;
 
     event ExecutorUpdated(address executor, bool allowed);
+    event CallFailed(bytes32 callId, address executor);
 
-    constructor(address escrow_, address owner_, address[] memory executors) Ownable(owner_) {
-        escrow = escrow_;
+    error InvalidExecutor(address executor);
+
+    constructor(address owner_, address[] memory executors) Ownable(owner_) {
         for (uint256 i; i < executors.length; i++) {
             isExecutor[executors[i]] = true;
             emit ExecutorUpdated(executors[i], true);
@@ -20,27 +27,24 @@ contract Operator is Ownable {
     }
 
     modifier onlyExecutor() {
-        if (msg.sender != address(this) && isExecutor[msg.sender]) revert();
+        if (isExecutor[msg.sender]) revert InvalidExecutor(msg.sender);
         _;
     }
 
-    function multicall(bytes[] calldata calls) external onlyExecutor {
-        uint256 callsLen = calls.length;
-        for (uint256 i; i < callsLen; i++) {
-            address(this).call(calls[i]);
+    function execute(Call calldata call) external onlyExecutor {
+        _execute(call);
+    }
+
+    function execute(Call calldata calls) external onlyExecutor {
+        uint256 len = calls.length;
+        for (uint256 i; i < len; i++) {
+            _execute(calls[i]);
         }
     }
 
-    function execute(bytes calldata data) external onlyExecutor {
-        escrow.call(data);
-    }
-
-    function execute(address target, bytes calldata data) external onlyExecutor {
-        target.call(data);
-    }
-
-    function execute(address target, uint256 value, bytes calldata data) external onlyExecutor {
-        target.call{value: value}(data);
+    function _execute(Call calldata call) internal {
+        (bool success, bytes memory res) = call.target.call(call.data);
+        if (!success) emit CallFailed(call.id, msg.sender);
     }
 
     function updateExecutor(address executor, bool allowed) external onlyOwner {
